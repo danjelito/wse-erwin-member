21186131d661768a86eac8fa0fc149e9086ac299
Devan Anjelito <anjelitodevan@gmail.com>
2024-01-03 13:08:34 +0700

    format module

--- modules/module.py
+++ modules/module.py
@@ -2,50 +2,6 @@ import pandas as pd
 import numpy as np
 
 
-def is_active(
-    df: pd.DataFrame,
-    start_date_col: pd.Timestamp,
-    end_date_col: pd.Timestamp,
-    lower_bound: str,
-):
-    """
-    Returns boolean to indicate if a member is active in a certain timeframe.
-    By default, the timeframe is one month,
-    starting from the lower_bound to the upper_bound
-    and inclusive
-
-    Arguments:
-        df -- dataframe
-        start_date -- contract start date of the member
-        end_date -- contract end date of the member
-        lower_bound -- lower bound of the timeframe
-
-    Returns:
-        boolean indicating if the member is active on the timeframe
-    """
-
-    lower_bound = pd.to_datetime(lower_bound)
-    upper_bound = lower_bound + pd.offsets.MonthEnd(0)
-
-    conditions = (
-        ((df[start_date_col] <= lower_bound) & (df[end_date_col] >= upper_bound)),
-        (
-            (df[start_date_col] <= lower_bound)
-            & (df[end_date_col] <= upper_bound)
-            & (df[end_date_col] >= lower_bound)
-        ),
-        (
-            (df[start_date_col] >= lower_bound)
-            & (df[start_date_col] <= upper_bound)
-            & (df[end_date_col] >= upper_bound)
-        ),
-        ((df[start_date_col] >= lower_bound) & (df[end_date_col] <= upper_bound)),
-        True,
-    )
-    choices = [True, True, True, True, False]
-    return np.select(conditions, choices, default=False)
-
-
 to_rename = {
     "date_of_birth": "dob",
     "from": "start_date",
@@ -97,3 +53,47 @@ income_cat = pd.CategoricalDtype(
     ],
     ordered=True,
 )
+
+
+def is_active(
+    df: pd.DataFrame,
+    start_date_col: pd.Timestamp,
+    end_date_col: pd.Timestamp,
+    lower_bound: str,
+):
+    """
+    Returns boolean to indicate if a member is active in a certain timeframe.
+    By default, the timeframe is one month,
+    starting from the lower_bound to the upper_bound
+    and inclusive
+
+    Arguments:
+        df -- dataframe
+        start_date -- contract start date of the member
+        end_date -- contract end date of the member
+        lower_bound -- lower bound of the timeframe
+
+    Returns:
+        boolean indicating if the member is active on the timeframe
+    """
+
+    lower_bound = pd.to_datetime(lower_bound)
+    upper_bound = lower_bound + pd.offsets.MonthEnd(0)
+
+    conditions = (
+        ((df[start_date_col] <= lower_bound) & (df[end_date_col] >= upper_bound)),
+        (
+            (df[start_date_col] <= lower_bound)
+            & (df[end_date_col] <= upper_bound)
+            & (df[end_date_col] >= lower_bound)
+        ),
+        (
+            (df[start_date_col] >= lower_bound)
+            & (df[start_date_col] <= upper_bound)
+            & (df[end_date_col] >= upper_bound)
+        ),
+        ((df[start_date_col] >= lower_bound) & (df[end_date_col] <= upper_bound)),
+        True,
+    )
+    choices = [True, True, True, True, False]
+    return np.select(conditions, choices, default=False)
